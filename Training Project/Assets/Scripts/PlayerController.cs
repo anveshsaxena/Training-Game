c7310f9a4e825210e0044194d0814a4d8878ae54
Anvesh Saxena <anvsh@live.in>
2022-04-14 23:59:36 +0800

    Added Panels

--- Training Project/Assets/Scripts/PlayerController.cs
+++ Training Project/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rigidbody2D;
 
+    public GameObject gameWonPanel;
+    public GameObject pausedPanel;
+    public GameObject gameLostPanel;
+
     public float speed;
+    private bool levelStatus;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,26 +22,38 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxis("Horizontal") > 0)
+        if (!levelStatus)
         {
-            rigidbody2D.velocity = new Vector2(speed, 0f);
-        }
-        else if(Input.GetAxis("Horizontal") < 0)
-        {
-            rigidbody2D.velocity = new Vector2(-speed, 0f);
-        }
-        else if (Input.GetAxis("Vertical") > 0)
-        {
-            rigidbody2D.velocity = new Vector2(0f, speed);
-        }
-        else if (Input.GetAxis("Vertical") < 0)
-        {
-            rigidbody2D.velocity = new Vector2(0f, -speed);
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                rigidbody2D.velocity = new Vector2(speed, 0f);
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                rigidbody2D.velocity = new Vector2(-speed, 0f);
+            }
+            else if (Input.GetAxis("Vertical") > 0)
+            {
+                rigidbody2D.velocity = new Vector2(0f, speed);
+            }
+            else if (Input.GetAxis("Vertical") < 0)
+            {
+                rigidbody2D.velocity = new Vector2(0f, -speed);
+            }
+            else if (Input.GetAxis("Cancel") > 0)
+            {
+                pausedPanel.SetActive(true);
+            }
+            else
+            {
+                rigidbody2D.velocity = new Vector2(0f, 0f);
+            }
         }
         else
         {
             rigidbody2D.velocity = new Vector2(0f, 0f);
         }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -43,7 +61,22 @@ public class PlayerController : MonoBehaviour
         if(other.tag == "Astronaut")
         {
             Debug.Log("Level Complete!!");
+            gameWonPanel.SetActive(true);
+            levelStatus = true;
         }
-        
+        else if(other.tag == "Asteroid")
+        {
+            Debug.Log("Ship Destroyed");
+            gameLostPanel.SetActive(true);
+            levelStatus = true;
+        }
     }
+
+    public void RestartGame()
+    {
+        Debug.Log("Button Clicked");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
 }
